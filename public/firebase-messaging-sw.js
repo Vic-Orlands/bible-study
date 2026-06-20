6c172989e11a63dd86291f7a506d0f72ce541960
Chimezie Innocent <chimezieinnocent39@gmail.com>
2026-06-20 14:09:02 +0100

    fix: load Firebase messaging config at runtime

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,12 +1,10 @@
 importScripts("https://www.gstatic.com/firebasejs/12.15.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.15.0/firebase-messaging-compat.js");
 
-firebase.initializeApp({
-  apiKey: "REMOVED_FIREBASE_API_KEY",
-  appId: "1:1039505711205:web:e089f30f2cad94c9c5668c",
-  authDomain: "bible-study-5d5f1.firebaseapp.com",
-  messagingSenderId: "1039505711205",
-  projectId: "bible-study-5d5f1",
-});
-
-firebase.messaging();
+fetch("/api/firebase-config")
+  .then((response) => response.json())
+  .then((config) => {
+    firebase.initializeApp(config);
+    firebase.messaging();
+  })
+  .catch((error) => console.error("Failed to initialize Firebase messaging:", error));
